a63eb2ca3933b1be5d098bd34b5011e2694b26fa
Raman Dzianishchyk <Raman_Dzianishchyk@dancosoft.com>
2015-04-05 01:48:02 +0300

    Added test moving for player. (Only for the study)

---
+++ Space/Assets/Scripts/Player/Player2DControll.cs
@@ -0,0 +1,42 @@
+﻿namespace Assets.Scripts.Player
+{
+    using JetBrains.Annotations;
+
+    using UnityEngine;
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Player2DControll : MonoBehaviour
+    {
+        public float ForwardMovingForce = 1;
+        public float RotationForce = 1;
+
+        [NotNull]
+        private Rigidbody2D m_rigidbody2D;
+
+        // Use this for initialization
+        private void Start()
+        {
+            this.m_rigidbody2D = this.GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                this.m_rigidbody2D.AddForce(new Vector2(0, this.ForwardMovingForce));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                this.m_rigidbody2D.transform.Rotate(0, 0, this.RotationForce);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                this.m_rigidbody2D.AddForce(new Vector2(0, -this.ForwardMovingForce));
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.m_rigidbody2D.transform.Rotate(0, 0, -this.RotationForce);
+            }
+        }
+    }
+}
